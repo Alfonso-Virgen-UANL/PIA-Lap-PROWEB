e972d3569753ded031cd51bae747b318816dd6a1
Alfonso-Virgen-UANL <alfonso.virgend@uanl.edu.mx>
2024-11-18 15:52:59 -0600

    Agregado reseñas de usuario al perfil

---
+++ PaginasUsuario/script.js
@@ -0,0 +1,27 @@
+$(function crearTarjeta(registro) {
+    $("#btn1").click(function() {
+        if (confirm("¿Estás seguro de que quieres eliminar esta reseña?")) {
+            $("#tarjeta1").hide(300);
+        }
+    });
+    $("#btn2").click(function() {
+        if (confirm("¿Estás seguro de que quieres eliminar esta reseña?")) {
+            $("#tarjeta2").hide(300);
+        }
+    });
+    $("#btn3").click(function() {
+        if (confirm("¿Estás seguro de que quieres eliminar esta reseña?")) {
+            $("#tarjeta3").hide(300);
+        }
+    });
+    $("#btn4").click(function() {
+        if (confirm("¿Estás seguro de que quieres eliminar esta reseña?")) {
+            $("#tarjeta4").hide(300);
+        }
+    });
+    $("#btn5").click(function() {
+        if (confirm("¿Estás seguro de que quieres eliminar esta reseña?")) {
+            $("#tarjeta5").hide(300);
+        }
+    });
+});
